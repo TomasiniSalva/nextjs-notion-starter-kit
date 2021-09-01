cf1a38d66863938ecba76a5942493dad77d793ad
TomasiniSalva <88755540+TomasiniSalva@users.noreply.github.com>
2021-09-01 15:38:17 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'e9c6381fc0f14b488c57cabfbc2ec28e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '草榴社區 1024 邀请码',
+  domain: 'fotski.com',
+  author: '草榴社區 1024 邀请码',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '草榴社區,草榴,草榴社区邀请码,草榴邀请码,1024 邀请码',
+  socialImageTitle: '草榴社區邀请码',
+  socialImageSubtitle: '草榴邀请码! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '0W7J8xujiE3mlEB',
+  github: '',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
